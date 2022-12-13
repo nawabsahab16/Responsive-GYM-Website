e363423ecc100dba2c8d31718b738b9afb2f1a7d
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2022-12-13 22:14:55 +0530

    50%

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,47 @@ const navMenu = document.getElementById('nav-menu'),
                            : header.classList.remove('bg-header')
     }
     window.addEventListener('scroll, scrollHeader')
+
+
+
+
+
+    const calculateForm = document.getElementById('calculate-form'),
+          calculateCm = document.getElementById('calculate-cm'),
+          calculateKg = document.getElementById('calculate-kg'),
+          calculateMessage = document.getElementById('calculate-message')
+
+    const calculateBmi = (e) =>{
+        e.preventDefault()
+
+        if(calculateCm.value === '' || calculateKg.value === ''){
+
+            calculateMessage.classList.remove('color-green')
+            calculateMessage.classList.add('color-red')
+
+             calculateMessage.textContent = 'Fill in the Height and Weight ☺️ '
+
+
+
+             setTimeout(()=>{
+                calculateMessage.textContent=''
+             } , 3000)
+        } else{
+
+            const cm = calculate.value/100,
+                  kg = calculateKg.value,
+                  bmi = Math.round(kg/(cm * cm))
+
+            
+            if(bmi < 18.5){
+                calculateMessage.classList.add('color-green')
+                calculateMessage.textContent = ' Your BMI is ${bmi} and you are skinny '
+            }      
+            else if(bmi < 25){
+                calculateMessage.classList.add('color-green')
+                calculateMessage.textContent = 'Your BMI is ${bmi} and you are healthy '
+            }
+        }
+    }      
+
+    calculateForm.addEventListener('submit', calculateBmi)
